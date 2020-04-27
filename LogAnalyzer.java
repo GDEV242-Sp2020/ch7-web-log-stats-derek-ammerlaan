a462b48e2767ab2f50f1307178c32917f33818cd
Derek Ammerlaan <18dammerlaan@gmail.com>
2020-04-27 13:48:28 -0400

    Completed 7.12 thru 7.15 and tested features

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -22,6 +22,20 @@ public class LogAnalyzer
         // Create the reader to obtain the data.
         reader = new LogfileReader();
     }
+    
+    /**
+     * Create a LogFileReader that will supply data
+     * from a particular log file
+     * @param logFileName The file of log data
+     */
+    public LogAnalyzer(String logFileName)
+    { 
+        // Create the array object to hold the hourly
+        // access counts.
+        hourCounts = new int[24];
+        // Create the reader to obtain the data.
+        reader = new LogfileReader(logFileName);
+    }
 
     /**
      * Analyze the hourly access data from the log file.
@@ -55,4 +69,31 @@ public class LogAnalyzer
     {
         reader.printData();
     }
+    
+    /*
+     * Return the number of accesses recorded in the log file.
+     */
+    public int numberOfAccesses()
+    {
+        int total = 0;
+        for(int i = 0; i < hourCounts.length; i++)
+        {
+            total += hourCounts[i];
+        }
+        return total;
+    }
+    
+    public int busiestHour()
+    {
+      int busiest = 0;
+      for(int i = 0; i < hourCounts.length; i++)
+      {
+         if(hourCounts[busiest] < hourCounts[i])
+         {
+             busiest = i;
+         }
+      }
+      return busiest;
+    }
+    
 }
